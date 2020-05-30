c218150a63d9a24313f3e9a225c6c64fc95949f1
Bailey Niemiller <baileyniemiller@gmail.com>
2020-05-29 22:29:36 -0500

    Second commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,43 @@
 $(document).ready(readyNow);
 
 function readyNow(){
-console.log('JQ is working!')
+    console.log('JQ is working!');
+    $('.equals').on('click', startCalculation);
 }
+
+const add = $('#add');
+const subtract = $('#subtract');
+const multiply = $('#multiply');
+const divide = $('#divide');
+const total = $('#total');
+
+
+function startCalculation() {
+    const number1In = $('#number1In').val();
+    const number2In = $('#number2In').val();
+    const numbersIn = {
+        number1 = number1In,
+        number2 = number2In,
+        operator = '',
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/calculate',
+        data: numbersIn,
+    }).then(function (response) {
+        console.log('Numbers have been received!');
+
+    }).catch(function (response) {
+        console.log('Sorry, error in retrieving numbers.');
+    });
+}
+
+
+
+   // $('#number1In').val('');
+   // $('#number2In').val('');
+   // let runningList = $('.totalList');
+   // runningList.empty();
+   // $('.totalList').append(`<li> ${numbersIn.number1} + ${operator} + ${numbersIn.number2} = ${numbersIn.result} </li>`);
+   // console.log(numbersIn);
+
--- server/server.js
+++ server/server.js
@@ -9,39 +9,66 @@ app.use(bodyParser.urlencoded({extended:true}));
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-app.listen(PORT, () => {
-    console.log('Server is running on port', PORT)
-});
-
 //////////////////////////////////////////////////
 
-
-let numbersInput = [
-    {
-        number1: 0,
-        number2: 0,
-        result: 0,
-    }
-]
-
 let numbersOutput = [];
-
 let numbersHistory = [];
 
+
+
+// calculate();
+
+
 //add a POST route to allow new numbers to be calculated
-app.post('/calculate', (req, res) => {
+// this is an HTTP handler for POST /calculate
+app.post('/calculate', function(req, res) {
+    // req: express built this based on what the browser or postman sent us!
+    // res: express built this, our job is to send something back using res.send();
+    // req: short for "http request" (from client)
+    // res: short for "http response" (to the client)
+    /*  req.body:
+        {
+            number1: 10, (could be "10")
+            number2: 20, (could be "20")
+            operator: "+"
+        }
+    */
+    const obj = req.body;
+    console.log(Number(obj.number1));
+    // obj is just an object {bread: 'bagel'}
+
+    console.log(obj);
+
+
+    let operator = obj.operator;
+    let result = 0;
+    // let total = req.body.total;
+    let number1 = Number(obj.number1);
+    let number2 = Number(obj.number2);
     console.log('Calculating...calculating...');
-    res.send(numbersInput);
+    console.log(obj);
+    if (operator === '+') {
+        result = number1 + number2;
+        numbersOutput.push(result);
+    } else {
+        res.sendStatus(400);
+        return;
+    }
+    res.send({total: result});
 });
 
 //add a GET route to get back the result
-app.get('/result', (req, res) => {
+app.get('/result', function(req, res) {
     console.log('Getting the result!');
     res.send(numbersOutput);
 });
 
 //add another GET route to get the history of calculations
-app.get('/history', (req, res) => {
+app.get('/history', function(req, res) {
     console.log('We\'d like to see a history array.');
     res.send(numbersHistory);
 })
+
+app.listen(PORT, function() {
+    console.log('Server is running on port', PORT)
+});
