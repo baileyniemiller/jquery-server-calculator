15061a5ab96ed4256816eb770309601fe2b01f97
Bailey Niemiller <baileyniemiller@gmail.com>
2020-06-02 11:54:12 -0500

    quick update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-// const PORT = 5000;
 
 // Must be added before GET and POST routes.
 app.use(bodyParser.urlencoded({extended:true}));
@@ -85,7 +84,7 @@ app.get('/history', function(req, res) {
 
 // run on the environment variable PORT (like heroku gives us)
 // otherwise just run on port 5000
-const port = provess.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log('Server is running on port', PORT)
 });
